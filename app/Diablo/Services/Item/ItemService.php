a391a840d6239801d1303017f4b342de9ebb15b8
John Leider <john.j.leider@gmail.com>
2016-02-01 22:43:28 -0500

    short term work around for defining all finger items as finger.

--- app/Diablo/Services/Item/ItemService.php
+++ app/Diablo/Services/Item/ItemService.php
@@ -14,11 +14,16 @@ class ItemService
      */
     public function saveItem($item)
     {
+        // I know this sucks, will fix later -zs
+        $slot = $item->slots[0] == 'left-finger'
+            ? 'finger'
+            : $item->slots[0];
+
         $item_array = [
             'name' => $item->name,
             'battlenet_item_id' => $item->id,
             'display_color' => $item->displayColor,
-            'slot' => $item->slots[0]
+            'slot' => $slot
         ];
 
         return Item::firstOrCreate($item_array);
