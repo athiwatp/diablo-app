9417cdfb46fd3245fbf640f253e096a5c487ea7b
John Leider <john.j.leider@gmail.com>
2016-02-02 20:31:46 -0500

    brought in d3tooltip.js file and updated attribute call to try catch as it was causing errors in some cases.  Updated gulpfile to merge heroes index.js with d3tooltip.  Removed include of d3tooltips on show template.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,11 @@ elixir(function(mix) {
     mix.sass('index.scss');
 
     mix.browserify('pages/home/index.js', 'public/js/pages/home/index.js');
-    mix.browserify('pages/heroes/index.js', 'public/js/pages/heroes/index.js');
+    mix.browserify('pages/heroes/index.js', resources + 'build/js/pages/heroes/index.js');
+    mix.scripts([
+        'build/js/pages/heroes/index.js',
+        'js/d3tooltip.js'
+    ], 'public/js/pages/heroes/index.js', resources);
 
     mix.copy('resources/assets/img', 'public/img');
 });
--- resources/views/heroes/show.blade.php
+++ resources/views/heroes/show.blade.php
@@ -6,5 +6,4 @@
 
 @section('js')
     <script src="/js/pages/heroes/index.js" type="text/javascript"></script>
-    <script src="http://www.diabloprogress.com/js1/d3-tooltips.js"></script>
 @stop
