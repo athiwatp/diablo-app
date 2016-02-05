338c9e6215a957c595197064e3a805cdbd8947a1
John Leider <john.j.leider@gmail.com>
2016-02-04 21:48:23 -0500

    Removed no longer needed index files for pages.

--- resources/assets/js/pages/home/index.js
+++
@@ -1,6 +0,0 @@
-var Vue = require('vue');
-Vue.use(require('vue-resource'));
-Vue.config.debug = true;
-window.Vue = Vue;
-
-new Vue(require('./app.vue')).$mount('#app');
--- resources/assets/js/pages/profile/index.js
+++
@@ -1,5 +0,0 @@
-var Vue = require('vue');
-Vue.config.debug = true;
-window.Vue = Vue;
-
-new Vue(require('./app.vue')).$mount('#app');
