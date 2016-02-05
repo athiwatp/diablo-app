4f4774b06ae1b90fdaef43a74cde56fe65d428a1
John Leider <john.j.leider@gmail.com>
2016-02-04 21:47:31 -0500

    Updated to conform to new javascript structure.  Added output naming for assets.

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,18 @@ var resources = 'resources/assets/';
 
 elixir(function(mix) {
     // Scss
-    mix.sass('index.scss')
-    
+    mix.sass('index.scss', 'app.css')
+
     // JS libs
     .scripts([
-        'd3tooltip.js'
+        'libs/d3tooltip.js'
     ], 'public/js/libs.js')
 
     // Misc
     .copy('resources/assets/img', 'public/img')
 
-    // Pages
-    .browserify('pages/home/index.js', 'public/js/pages/home/index.js')
-    .browserify('pages/heroes/index.js', 'public/js/pages/heroes/index.js')
+    // Browserify
+    .browserify('index.js', 'public/js/app.js')
 
     // Browsersync
     .browserSync();
