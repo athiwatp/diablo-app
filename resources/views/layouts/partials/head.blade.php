40a0367c8944259cafdf5a5a2ed4dcca4dc2c807
John Leider <john.j.leider@gmail.com>
2016-02-05 23:01:55 -0500

    added material design icons to head.

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -8,5 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="icon" type="image/png" href="/img/diablorankings.png">
 </head>
