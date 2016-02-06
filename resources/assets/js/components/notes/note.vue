baae4cf8b8a0dda97e24d27a03820f4a7d8a9cd0
John Leider <john.j.leider@gmail.com>
2016-02-05 22:06:08 -0500

    Note component.

---
+++ resources/assets/js/components/notes/note.vue
@@ -0,0 +1,35 @@
+<style lang="scss">
+    @import '../../../sass/_variables.scss';
+
+    .alert {
+        border-radius: 0px;
+        background-color: $tertiary-color;
+        border: $tertiary-color;
+        color: $primary-color;
+    }
+</style>
+
+<template>
+	<div class="alert" :class="class" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+		<slot></slot>
+    </div>
+</template>
+
+<script>
+	export default {
+		computed: {
+			class () {
+				switch (this.type) {
+					case 'success':
+						return 'alert-success';
+						break;
+				}
+			}
+		},
+
+		props: ['type']
+	}
+</script>
