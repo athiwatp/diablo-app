22d2fd6ec7d57818c81dc8f1437043ef2b8f2049
John Leider <john.j.leider@gmail.com>
2016-02-05 23:02:17 -0500

    Migrations for icon field in skills/items.

---
+++ database/migrations/2016_02_06_035047_add_icon_to_skills_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIconToSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            $table->string('icon')->after('tool_tip_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('skills', function (Blueprint $table) {
+            $table->dropColumn('icon');
+        });
+    }
+}
