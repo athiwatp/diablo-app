d8532390fdb685a90c141a28cca73200151e4e7f
John Leider <john.j.leider@gmail.com>
2016-02-06 20:26:43 -0500

    Cleaned up home vue template.  Added leaderboard-card.

--- resources/assets/js/pages/home/home.vue
+++ resources/assets/js/pages/home/home.vue
@@ -1,13 +1,5 @@
 <style lang="scss">
-    section h2 {
-        text-align: center;
-        color: #fff;
-        text-transform: uppercase;
-    }
-
-    .container h2 {
-        margin: 2rem 0;
-    }
+    @import './_home.scss';
 </style>
 
 <template>
@@ -20,81 +12,27 @@
             </jumbo>
         </header>
 
-        <div class="container">
+        <div id="home"
+             class="container"
+        >
             <section class="leaderboards-section">
                 <div class="row">
-                    <h2 class="col-md-12">Leaderboards</h2>
+                    <h2 class="col-md-12">Season Leaderboards</h2>
 
                     <div class="col-md-6">
-                        <div class="card card--diablo text-xs-right">
-                            <img :src="softcoreTop" alt="" class="card-img-top">
-
-                            <div class="card-img-overlay text-xs-left">
-                                <h3 class="card-title text-uppercase">Softcore</h3>
-                                <h3 class="text--tertiary">{{ state.softcore.top.rift_level }}</h3>
-                                <h4 class="text--secondary">{{ state.softcore.top.hero.name }}</h4>
-                                <div class="card-text">
-                                    <a href="/heroes/{{ state.softcore.top.hero.id }}" class="btn btn--secondary">Go to Hero</a>
-                                </div>
-                            </div>
-
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item list-group-item--diablo"
-                                    v-for="ranking in state.softcore.ladder"
-                                >
-                                    <div class="row">
-                                        <div class="col-md-3 text-xs-center">
-                                            <span class="label label--diablo">{{ ranking.rift_level }}</span>
-                                        </div>
-                                        <div class="col-md-4 text-xs-center text--{{ ranking.class.replace(' ', '-') }}">
-                                            {{ ranking.class }}
-                                        </div>
-                                        <div class="col-md-5 text-md-right text-xs-center">
-                                            <a href="/heroes/{{ ranking.hero.id }}">{{ ranking.hero.name }}</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="list-group-item text-xs-center">
-                                    <a href="#" class="btn btn--secondary-outline">View All</a>
-                                </li>
-                            </ul>
-                        </div>
+                        <leaderboard-card :leaderboard="state.softcore"
+                                          orientation="left"
+                        >
+                            Softcore
+                        </leaderboard-card>
                     </div>
 
                     <div class="col-md-6">
-                        <div class="card card--diablo text-xs-left">
-                            <img :src="hardcoreTop" alt="" class="card-img-top">
-
-                            <div class="card-img-overlay text-xs-right">
-                                <h3 class="card-title text-uppercase">Hardcore</h3>
-                                <h3 class="text--tertiary">{{ state.hardcore.top.rift_level }}</h3>
-                                <h4 class="text--secondary">{{ state.hardcore.top.hero.name }}</h4>
-                                <div class="card-text">
-                                    <a href="/heroes/{{ state.hardcore.top.hero.id }}" class="btn btn--secondary">Go to Hero</a>
-                                </div>
-                            </div>
-
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item list-group-item--diablo"
-                                    v-for="ranking in state.hardcore.ladder"
-                                >
-                                    <div class="row">
-                                        <div class="col-md-3 text-xs-center">
-                                            <span class="label label--diablo">{{ ranking.rift_level }}</span>
-                                        </div>
-                                        <div class="col-md-4 text-xs-center text--{{ ranking.class.replace(' ', '-') }}">
-                                            {{ ranking.class }}
-                                        </div>
-                                        <div class="col-md-5 text-md-right text-xs-center">
-                                            <a href="/heroes/{{ ranking.hero.id }}">{{ ranking.hero.name }}</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="list-group-item text-xs-center">
-                                    <a href="#" class="btn btn--secondary-outline">View All</a>
-                                </li>
-                            </ul>
-                        </div>
+                        <leaderboard-card :leaderboard="state.hardcore"
+                                          orientation="right"
+                        >
+                            Hardcore
+                        </leaderboard-card>
                     </div>
                 </div>
             </section>
@@ -159,6 +97,7 @@
     import mainFooter from '../../components/main-footer/main-footer.vue';
     import jumbo from '../../components/jumbotron/jumbo.vue';
     import classCard from '../../components/cards/class.vue';
+    import leaderboardCard from './components/leaderboard-card/leaderboard-card.vue';
 
     export default {
         data () {
@@ -167,20 +106,7 @@
             }
         },
 
-        computed: {
-            softcoreTop () {
-                if (typeof this.state.softcore.top.class != 'undefined') {
-                    return BASE_URL + '/img/' + this.state.softcore.top.class + '/crest.png';
-                }
-            },
-            hardcoreTop () {
-                if (typeof this.state.hardcore.top.class != 'undefined') {
-                    return BASE_URL + '/img/' + this.state.hardcore.top.class + '/crest.png';
-                }
-            }
-        },
-
-        components: { mainNavbar, mainFooter, jumbo, classCard },
+        components: { mainNavbar, mainFooter, jumbo, classCard, leaderboardCard },
 
         ready () {
             homeStore.get(function () {
