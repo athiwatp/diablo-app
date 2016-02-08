e957203b2f2ba38919731d0c92bde0e25b449261
John Leider <john.j.leider@gmail.com>
2016-02-08 12:32:36 -0500

    Import filters and add to vue.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,14 @@ import Vue from 'vue';
 import vueResource from 'vue-resource';
 import app from './app.vue';
 
+import {battleTag, classPortrait, classText} from './filters/filters.js';
+
 Vue.use(vueResource);
+
+Vue.filter('battleTag', battleTag);
+Vue.filter('classPortrait', classPortrait);
+Vue.filter('classText', classText);
+
 Vue.config.debug = true;
 
 window.$ = $;
