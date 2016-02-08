dd92dbdd03651cfc05b703a652040edd753d2623
John Leider <john.j.leider@gmail.com>
2016-02-08 12:30:10 -0500

    Added leaderboard routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,10 +5,13 @@ Route::group(['middleware' => 'api', 'prefix' => 'api'], function () {
 
     Route::get('heroes/{hero}', 'HeroController@showApi');
     Route::patch('heroes/{hero}', 'HeroController@update');
+    Route::get('leaderboards/{type}', 'LeaderboardsController@data');
+    Route::get('leaderboards/search', 'LeaderboardsController@search');
 });
 
 Route::group(['middleware' => 'web'], function () {
     Route::get('/', 'HomeController@index');
 
     Route::get('heroes/{hero}', 'HeroController@show');
+    Route::get('leaderboards/{type?}', 'LeaderboardsController@index');
 });
