a31cd9d090bf55babac6fcd0c4e5e08430bec262
John Leider <john.j.leider@gmail.com>
2016-02-14 23:18:38 -0500

    Updated navbar style.

--- resources/assets/js/components/main-navbar/main-navbar.vue
+++ resources/assets/js/components/main-navbar/main-navbar.vue
@@ -4,17 +4,26 @@
 
 <template>
     <nav class="navbar navbar-light bg-faded navbar-full navbar--diablo">
-        <a href="/" class="navbar-brand">
-            <img :src="logo" alt="">
-        </a>
-        <ul class="nav navbar-nav">
-            <li class="nav-item">
-                <a href="/" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-                <a href="/leaderboards" class="nav-link">Leaderboards</a>
-            </li>
-        </ul>
+        <div class="container">
+            <a href="/" class="navbar-brand">
+                <img :src="logo" alt="">
+                <span class="navbar-brand__text">
+                    D<span class="navbar-brand__subtext">iablo</span>
+                    R<span class="navbar-brand__subtext">ankings</span>
+                </span>
+            </a>
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a href="/" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/leaderboards" class="nav-link">Leaderboards</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/profiles" class="nav-link">Profiles</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
