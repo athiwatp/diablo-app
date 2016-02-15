aba18031dbc182c94733cb2c61be96a79693868f
John Leider <john.j.leider@gmail.com>
2016-02-15 15:37:06 -0500

    Updated main-footer.

--- resources/assets/js/components/main-footer/main-footer.vue
+++ resources/assets/js/components/main-footer/main-footer.vue
@@ -1,41 +1,41 @@
 <style lang="scss">
-    @import "../../../sass/_variables.scss";
-
-    footer {
-        background-color: $secondary-color;
-        padding: 1rem 0;
-        margin-top: 2rem;
-    }
-
-    footer a {
-        color: #fff;
-    }
-    
-    footer .container {
-        margin-top: 0;
-    }
+    @import './main-footer';
 </style>
 
 <template>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <ul class="list-unstyled m-t-0 m-b-0">
-                        <li>
-                            <a href="#">
-                                <i class="fa fa-envelope"></i> Contact
-                            </a>
-                            <a href="#" class="m-l-1">
-                                <i class="fa fa-twitter"></i> Twitter
-                            </a>
-                        </li>
-                    </ul>
+    <div class="bottom-banner">
+        <div class="bottom-banner__image"></div>
+        <div class="bottom-banner__text">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-xs-center">
+                        <h4>Connect with <strong>Diablo Rankings</strong></h4>
+                    </div>
                 </div>
-                <div class="col-md-6 text-xs-right text-white">
-                    &copy; Copyright 2016
+                <div class="row icons">
+                    <div class="col-md-6">
+                        <a href="#">
+                            <i class="fa fa-envelope"></i>
+                        </a>
+                        Contact me
+                    </div>
+                    <div class="col-md-6">
+                        <a href="#">
+                            <i class="fa fa-cc-paypal"></i>
+                        </a>
+                        Donate!
+                    </div>
                 </div>
             </div>
         </div>
-    </footer>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-xs-center text-white">
+                        John Leider &copy; 2016. All rights reserved
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 </template>
