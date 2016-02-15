4e679e36c5ce0f171341cd4d597f1bc0c9a7d27f
John Leider <john.j.leider@gmail.com>
2016-02-15 15:39:12 -0500

    Updated leaderboard card.

--- resources/assets/js/pages/home/components/leaderboard-card/leaderboard-card.vue
+++ resources/assets/js/pages/home/components/leaderboard-card/leaderboard-card.vue
@@ -6,6 +6,17 @@
         border-radius: 50%;
         border: 1px solid #7e7e7e;
     }
+    .caret {
+        display: inline-block;
+        width: 0px;
+        height: 0px;
+        margin-left: 2px;
+        vertical-align: middle;
+        border-top: 4px dashed;
+        border-right: 4px solid transparent;
+        border-left: 4px solid transparent;
+        margin-top: 10px;
+    }
 </style>
 
 <template>
@@ -16,7 +27,7 @@
             >
                 <span class="list-group-ranking__rank">{{ index + 1 }}</span>
                 <span class="list-group-ranking__rift">{{ ranking.rift_level }}</span>
-                <span class="list-group-ranking__class"> 
+                <span class="list-group-ranking__class hidden-sm-down"> 
                     <img :src="ranking | classPortrait"
                          alt=""
                          class="class-portrait"
@@ -25,11 +36,12 @@
                 </span>
                 <span class="list-group-ranking__hero">
                     {{ ranking.profile.battle_tag | battleTag }} {{ ranking.hero.clan_tag ? '&lt;' + ranking.hero.clan_tag + '&gt;' : '' }}
+                    <i class="caret pull-xs-right"></i>
                 </span>
             </li>
-            <div class="list-group-ranking__info row animated"
+            <div class="list-group-ranking__info row"
                  v-show="ranking.show"
-                 transition="fade"
+                 transition="expand"
             >
                 <div class="col-md-5 text-xs-center">
                     <img :src="ranking.class | topImage" alt="" class="img-fluid">
@@ -49,6 +61,10 @@
                             <span class="list-group-ranking__info__right">{{ ranking.hero.clan_name }}</span>
                         </li>
                     </ul>
+                </div>
+                <div class="col-md-12 text-xs-right">
+                    <a  href="/heroes/{{ ranking.hero.id }}"
+                        class="btn btn--secondary">Go to Hero Page</a>
                 </div>
             </div>
         </div>
