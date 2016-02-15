1f2eb118430f9d617efe674c37ed51b850d5bf2a
John Leider <john.j.leider@gmail.com>
2016-02-15 15:37:56 -0500

    Created top banner component.

---
+++ resources/assets/js/components/top-banner/top-banner.vue
@@ -0,0 +1,15 @@
+<style lang="scss">
+	@import './top-banner';
+</style>
+
+<template>
+	<div class="top-banner">
+        <div class="top-banner__image"></div>
+        <div class="top-banner__text">
+            <div class="container">
+                <h1>Diablo Rankings</h1>
+                <h6>Leaderboards, statistics and more</h6>
+            </div>
+        </div>
+    </div>
+</template>
