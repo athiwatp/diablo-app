2c218fe7d1958b8244e72578815ad9d597c4fa57
John Leider <john.j.leider@gmail.com>
2016-02-16 22:01:11 -0500

    Rebuilt footer using new banner component.

--- resources/assets/js/components/main-footer/main-footer.vue
+++ resources/assets/js/components/main-footer/main-footer.vue
@@ -3,28 +3,21 @@
 </style>
 
 <template>
-    <div class="bottom-banner">
-        <div class="bottom-banner__image"></div>
-        <div class="bottom-banner__text">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-xs-center">
-                        <h4>Connect with <strong>Diablo Rankings</strong></h4>
-                    </div>
+    <banner :parameters="footerBannerParameters">
+        <div class="container">
+            <h4>Connect with <strong>Diablo Rankings</strong></h4>
+            <div class="icons">
+                <div>
+                    <a href="#">
+                        <i class="fa fa-envelope"></i>
+                    </a>
+                    <p>Contact me</p>
                 </div>
-                <div class="row icons">
-                    <div class="col-md-6">
-                        <a href="#">
-                            <i class="fa fa-envelope"></i>
-                        </a>
-                        Contact me
-                    </div>
-                    <div class="col-md-6">
-                        <a href="#">
-                            <i class="fa fa-cc-paypal"></i>
-                        </a>
-                        Donate!
-                    </div>
+                <div>
+                    <a href="#">
+                        <i class="fa fa-cc-paypal"></i>
+                    </a>
+                    <p>Donate!</p>
                 </div>
             </div>
         </div>
@@ -37,5 +30,23 @@
                 </div>
             </div>
         </footer>
-    </div>
+    </banner>
 </template>
+
+<script>
+    import banner from '../banner/banner.vue';
+
+    export default {
+        data () {
+            return {
+                footerBannerParameters: {
+                    background: 'url("/img/footer.jpg") no-repeat fixed',
+                    backgroundSize: 'cover',
+                    backgroundPosition: '50% 250px'
+                }
+            }
+        },
+
+        components: {banner}
+    }
+</script>
