84d2cad7ed70e9cc81646f821fe75cb4c6e85246
John Leider <john.j.leider@gmail.com>
2016-02-16 22:02:31 -0500

    Created new banner component.

---
+++ resources/assets/js/components/banner/banner.vue
@@ -0,0 +1,20 @@
+<style lang="scss">
+    @import './banner';
+</style>
+
+<template>
+    <div class="banner">
+        <div class="banner__image"
+             :style="parameters"
+        ></div>
+        <div class="banner__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['parameters']
+    }
+</script>
