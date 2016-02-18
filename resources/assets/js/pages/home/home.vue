f9c68679f202e18b193fb1a962d6072c3574d82d
John Leider <john.j.leider@gmail.com>
2016-02-17 21:08:40 -0500

    Refactored structure of home page and finished final cleanup.

--- resources/assets/js/pages/home/home.vue
+++ resources/assets/js/pages/home/home.vue
@@ -13,66 +13,39 @@
             </div>
         </banner>
 
-        <section class="content">
-            <div class="leaderboard">
-                <div class="container-fluid leaderboard__rankings">
+        <div class="content">
+            <section class="leaderboard-rankings">
+                <div class="container-fluid">
                     <div class="row">
-                        <div class="col-sm-12 col-md-6">
-                            <h2 class="section-header">Softcore</h2>
-                            <leaderboard-card :leaderboard="state.softcore"
-                                              orientation="left"
-                            ></leaderboard-card>
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <h2 class="section-header section-header--left">Softcore</h2>
+                            <leaderboard-card :leaderboard="state.softcore"></leaderboard-card>
                         </div>
-                        <div class="col-sm-12 col-md-6">
-                            <h2 class="section-header">Hardcore</h2>
-                            <leaderboard-card :leaderboard="state.hardcore"
-                                              orientation="left"
-                            ></leaderboard-card>
+                        <div class="col-md-6 col-sm-12 col-xs-12">
+                            <h2 class="section-header section-header--right">Hardcore</h2>
+                            <leaderboard-card :leaderboard="state.hardcore"></leaderboard-card>
                         </div>
                     </div>
                 </div>
+            </section>
 
-                <div class="container-fluid leaderboard__classes">
-                    <div class="row section-wrapper">
-                        <div class="col-sm-12 col-md-12">
-                            <h2 class="section-header">Leaderboards</h2>
-                        </div>
-                    </div>
+            <h2 class="section-header">Classes</h2>
+            <section class="leaderboard-classes">
+                <div class="container-fluid">
                     <div class="row">
-                        <div class="col-md-2 col-sm-6 col-xs-12">
-                            <a href="#" class="leaderboard__classes__class">
-                                <img src="/img/barbarian/crest.png" alt="" class="img-fluid">
-                            </a>
-                        </div>
-                        <div class="col-md-2 col-sm-6 col-xs-12">
-                            <a href="#" class="leaderboard__classes__class">
-                                <img src="/img/crusader/crest.png" alt="" class="img-fluid">
-                            </a>
-                        </div>
-                        <div class="col-md-2 col-sm-6 col-xs-12">
-                            <a href="#" class="leaderboard__classes__class">
-                                <img src="/img/demon-hunter/crest.png" alt="" class="img-fluid">
-                            </a>
-                        </div>
-                        <div class="col-md-2 col-sm-6 col-xs-12">
-                            <a href="#" class="leaderboard__classes__class">
-                                <img src="/img/monk/crest.png" alt="" class="img-fluid">
-                            </a>
-                        </div>
-                        <div class="col-md-2 col-sm-6 col-xs-12">
-                            <a href="#" class="leaderboard__classes__class">
-                                <img src="/img/witch-doctor/crest.png" alt="" class="img-fluid">
-                            </a>
-                        </div>
-                        <div class="col-md-2 col-sm-6 col-xs-12">
-                            <a href="#" class="leaderboard__classes__class">
-                                <img src="/img/wizard/crest.png" alt="" class="img-fluid">
+                        <div class="col-md-2 col-sm-6 col-xs-12"
+                             v-for="class in classes"
+                        >
+                            <a href="#" 
+                               class="leaderboard-classes__class"
+                            >
+                                <img :src="class | classCrest">
                             </a>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
 
         <main-footer></main-footer>
     </div>
@@ -84,7 +57,6 @@
     import mainFooter from '../../components/main-footer/main-footer.vue';
     import leaderboardCard from './components/leaderboard-card/leaderboard-card.vue';
     import homeStub from '../../stubs/home';
-    import homeStore from '../../stores/home';
 
     export default {
         data () {
@@ -93,8 +65,11 @@
                 topBannerParameters: {
                     background: 'url("/img/bg.jpg") no-repeat fixed',
                     backgroundSize: 'cover',
-                    backgroundPosition: '50% -120px'
-                }
+                    backgroundPosition: '50% -66px'
+                },
+                classes: [
+                    'barbarian', 'crusader', 'demon-hunter', 'monk', 'witch-doctor', 'wizard'
+                ]
             }
         },
 
@@ -103,9 +78,13 @@
         components: { mainHeader, banner, mainFooter, leaderboardCard },
 
         ready () {
-            homeStore.get(() => {
-                this.state = homeStore.state;
-            });
+            this.init();
+        },
+
+        methods: {
+            init () {
+                this.state = JSON.parse(this.data);
+            }
         }
     }
 </script>
