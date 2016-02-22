15131a24fa586985c1b1ca36c1ae01fe05384203
John Leider <john.j.leider@gmail.com>
2016-02-21 22:30:54 -0500

    Updated navbar to accept page prop in order to determine active menu item.  Updated logo to have a nice effect with the site name.

--- resources/assets/js/components/main-navbar/main-navbar.vue
+++ resources/assets/js/components/main-navbar/main-navbar.vue
@@ -5,23 +5,36 @@
 <template>
     <nav class="nav">
         <div class="nav__container container">
-            <img :src="logo"
+            <a href="/"
+               class="nav__logo"
+            >
+                <img :src="logo"
                  alt="logo"
-                 class="nav__logo">
+                 class="nav__logo__img">
+                 <span class="nav__logo__text">Diablo Rankings</span>
+            </a>
             <ul class="nav__items">
-                <li class="nav__item">
-                    <a href="/leaderboards" class="nav__link">
+                <li class="nav__item"
+                    :class="{ 'nav__item--active': isActive('leaderboardPage') }"
+                >
+                    <a href="/leaderboards" 
+                       class="nav__link"
+                    >
                         Leaderboards
                         <span class="nav__link__subtext">rankings</span>
                     </a>
                 </li>
-                <li class="nav__item">
+                <li class="nav__item"
+                    :class="{ 'nav__item--active': isActive('profilePage') }"
+                >
                     <a href="/profiles" class="nav__link">
                         Profiles
                         <span class="nav__link__subtext">battlenet</span>
                     </a>
                 </li>
-                <li class="nav__item">
+                <li class="nav__item"
+                    :class="{ 'nav__item--active': isActive('heroPage') }"
+                >
                     <a href="/profiles" class="nav__link">
                         Heroes
                         <span class="nav__link__subtext">sanctuary</span>
@@ -42,9 +55,17 @@
     };
 
     export default {
+        props: ['page'],
+
         computed: {
             logo () {
                 return BASE_URL + '/img/diablorankings.png'
+            }
+        },
+
+        methods: {
+            isActive (page) {
+                return this.page == page;
             }
         }
     }
