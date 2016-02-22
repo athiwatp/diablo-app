acd524dd199a9119223d10d09ba4a93b74c701ef
John Leider <john.j.leider@gmail.com>
2016-02-21 22:35:14 -0500

    Updated component to use newly created styles.

--- resources/assets/js/pages/home/components/leaderboard-card/leaderboard-card.vue
+++ resources/assets/js/pages/home/components/leaderboard-card/leaderboard-card.vue
@@ -6,42 +6,30 @@
         border-radius: 50%;
         border: 1px solid #7e7e7e;
     }
-    .caret {
-        display: inline-block;
-        width: 0px;
-        height: 0px;
-        margin-left: 2px;
-        vertical-align: middle;
-        border-top: 4px dashed;
-        border-right: 4px solid transparent;
-        border-left: 4px solid transparent;
-        margin-top: 10px;
-    }
 </style>
 
 <template>
-    <ul class="list-group list-group-flush list-group-ranking">
+    <ul class="list">
         <template v-for="(index, ranking) in leaderboard.ladder">
-            <li class="list-group-item list-group-ranking__item"
+            <li class="list__item list__item--link"
                 @click="toggle($event)"
             >
-                <span class="list-group-ranking__rank">{{ index + 1 }}</span>
-                <span class="list-group-ranking__rift">{{ ranking.rift_level }}</span>
-                <span class="list-group-ranking__class hidden-sm-down"> 
+                <span class="flex-10">{{ index + 1 }}</span>
+                <span class="flex-20">{{ ranking.rift_level }}</span>
+                <span class="flex-30 hidden-sm-down"> 
                     <img :src="ranking | classPortrait"
                          alt="portrait"
                          class="class-portrait"
                     >
                     {{ ranking.class | capitalize }}
                 </span>
-                <span class="list-group-ranking__hero">
+                <span class="flex-40">
                     {{ ranking.profile.battle_tag | battleTag }} {{ ranking.hero.clan_tag ? '&lt;' + ranking.hero.clan_tag + '&gt;' : '' }}
                     <i class="caret pull-xs-right"></i>
                 </span>
             </li>
-            <div class="list-group-ranking__info row"
+            <div class="row"
                  v-show="ranking.show"
-                 transition="expand"
             >
                 <div class="col-sm-5 col-md-4 text-xs-center hidden-xs-down">
                     <img :src="ranking.class | classCrest" 
@@ -50,24 +38,29 @@
                     >
                 </div>
                 <div class="col-sm-7 col-md-8">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item list-group-ranking__info__item">
-                            <span class="list-group-ranking__info__left">Profile</span>
-                            <span class="list-group-ranking__info__right">{{ ranking.profile.battle_tag }}</span>
+                    <ul class="list">
+                        <a href="/profiles/{{ ranking.profile.id }}"
+                           class="list__item list__item--link"
+                        >
+                            <span class="flex-50">Profile</span>
+                            <span class="flex-50">{{ ranking.profile.battle_tag }}</span>
+                        </a>
+                        <li class="list__item">
+                            <span class="flex-50">Region</span>
+                            <span class="flex-50">{{ ranking.region }}</span>
                         </li>
-                        <li class="list-group-item list-group-ranking__info__item">
-                            <span class="list-group-ranking__info__left">Region</span>
-                            <span class="list-group-ranking__info__right">{{ ranking.region }}</span>
-                        </li>
-                        <li class="list-group-item list-group-ranking__info__item">
-                            <span class="list-group-ranking__info__left">Clan</span>
-                            <span class="list-group-ranking__info__right">{{ ranking.hero.clan_name }}</span>
+                        <li class="list__item">
+                            <span class="flex-50">Clan</span>
+                            <span class="flex-50">{{ ranking.hero.clan_name }}</span>
                         </li>
                     </ul>
                 </div>
                 <div class="col-sm-12 col-md-12 text-xs-right">
-                    <a  href="/heroes/{{ ranking.hero.id }}"
-                        class="btn btn--secondary">Go to Hero Page</a>
+                    <a href="/heroes/{{ ranking.hero.id }}"
+                       class="btn btn--secondary"
+                    >
+                       Go to Hero Page
+                    </a>
                 </div>
             </div>
         </template>
