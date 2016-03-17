01b08b53f7ded1b269d36cb82fbdfffafd688b68
John Leider <john.j.leider@gmail.com>
2016-03-16 22:18:17 -0400

    Naming convention change.

---
+++ resources/assets/js/pages/leaderboards/leaderboards-index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="page">
+        <main-header>
+            <banner :parameters="topBannerParameters"
+                    class="banner--slim"
+                    id="top-banner"
+            >
+                <h1>Seasonal Leaderboards</h1>
+            </banner>
+        </main-header>
+
+        <main-content>
+            <class-section></class-section>  
+            <team-section></team-section>
+        </main-content>
+    </div>
+</template>
+
+<script>
+    import classSection from '../../components/class-section/class-section.vue';
+    import teamSection from '../../components/team-section/team-section.vue';
+
+    export default {
+        data () {
+            return {
+                topBannerParameters: {
+                    background: 'url("/img/footer-banner.jpg") no-repeat fixed 50% 0'
+                }
+            }
+        },
+
+        components: {
+            classSection,
+            teamSection
+        }
+    }
+</script>
