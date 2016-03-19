f754bd62d4c0f18f8dca5c6b7c491b231de4b738
John Leider <john.j.leider@gmail.com>
2016-03-19 16:20:06 -0400

    Added additional column classes for all screen resolutions.

--- resources/assets/js/components/main-footer/main-footer.vue
+++ resources/assets/js/components/main-footer/main-footer.vue
@@ -26,7 +26,7 @@
         <div class="footer">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-12 text-xs-center text-white">
+                    <div class="col-md-12 col-xs-12 col-sm-12 text-xs-center text-white">
                         John Leider &copy; 2016. All rights reserved
                     </div>
                 </div>
