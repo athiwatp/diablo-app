2816dd0e131af674483dd070fa90ebfc857c31f7
John Leider <john.j.leider@gmail.com>
2016-03-20 11:26:09 -0400

    Turned back on crons for updating leaderboards.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command('leaderboard:update season 5 1000')
-//            ->twiceDaily(0, 12);
+        $schedule->command('leaderboard:update season 5 softcore')
+            ->twiceDaily(0, 12);
+        $schedule->command('leaderboard:update season 5 hardcore')
+            ->twiceDaily(1, 13);
     }
 }
