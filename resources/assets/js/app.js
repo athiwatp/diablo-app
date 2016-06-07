a0bbb3fcc514dacf69a4d27fa607042d732ca9f0
John Leider <john.j.leider@gmail.com>
2016-06-06 20:22:32 -0400

    Cleaned up main app.js.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,33 +1,34 @@
-import $ from 'jquery';
-import Vue from 'vue';
-import vueResource from 'vue-resource';
-import transitions from './transisions/transisions';
-import animations from './animations/animations';
-import filters from './filters/filters';
-import banner from './components/banner/banner.vue';
-import mainHeader from './components/main-header/main-header.vue';
-import mainContent from './components/main-content/main-content.vue';
+import $ from 'jquery'
+import Vue from 'vue'
+import Resource from 'vue-resource'
+import Transitions from './transisions/transisions'
+import Animations from './animations/animations'
+import Filters from './filters/filters'
+import App from './app.vue'
+import banner from './components/banner/banner.vue'
+import mainHeader from './components/main-header/main-header.vue'
+import mainContent from './components/main-content/main-content.vue'
 
-Vue.use(vueResource);
-Vue.config.debug = true;
+Vue.use(Resource)
+Vue.config.debug = true
 
-for (var key in transitions) {
-	Vue.transition(key, transitions[key]);
+for (let key in Transitions) {
+	Vue.transition(key, Transitions[key])
 }
 
-for (var key in filters) {
-	Vue.filter(key, filters[key]);
+for (let key in Filters) {
+	Vue.filter(key, Filters[key])
+}
+
+for (let key in Animations) {
+	Vue.component(key, Animations[key])
 }
 
 Vue.component('banner', banner);
 Vue.component('main-header', mainHeader);
 Vue.component('main-content', mainContent);
 
-for (var key in animations) {
-	Vue.component(key, animations[key]);
-}
-
 window.$ = $;
 window.Vue = Vue;
 
-new Vue(require('./app.vue'));
+new Vue(App);
